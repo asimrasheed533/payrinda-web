06c6039569f61a49a7f2a257647d04d4f258792c
Asim Rasheed <asimrasheed442@gmail.com>
2023-11-14 16:27:28 +0500

    Update Index.jsx

--- src/screens/Index.jsx
+++ src/screens/Index.jsx
@@ -1,3 +1,63 @@
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <>
+      <div className="welcome__main__container">
+        <div className="welcome__main__container__overlay__bg">
+          <svg
+            width="1134"
+            height="1005"
+            viewBox="0 0 1134 1005"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_f_201_615)">
+              <ellipse
+                cx="277.164"
+                cy="189.625"
+                rx="356"
+                ry="314.852"
+                fill="#012B84"
+                fill-opacity="0.17"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_f_201_615"
+                x="-578.836"
+                y="-625.228"
+                width="1712"
+                height="1629.71"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  stdDeviation="250"
+                  result="effect1_foregroundBlur_201_615"
+                />
+              </filter>
+            </defs>
+          </svg>
+        </div>
+        <div className="welcome__main__container__content">
+          <div className="welcome__main__container__content__title">
+            Welcome to payrinda!
+          </div>
+          <div className="welcome__main__container__content__heading">
+            Revolutionizing the Way You Experience <span>Payments</span>
+          </div>
+          <div className="welcome__main__container__content__sub__heading">
+            Bridging Possibilities, Simplifying Transactions
+          </div>
+        </div>
+        <div className="welcome__main__container__overlay__bg"></div>
+      </div>
+    </>
+  );
 }
