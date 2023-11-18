9bd228f29c35c1ea86db3679f459d62da2fe5f1d
Asim Rasheed <asimrasheed442@gmail.com>
2023-11-18 06:59:43 +0500

    Update BenefitCard.jsx

--- src/components/BenefitCard.jsx
+++ src/components/BenefitCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import Fade from "react-reveal/Zoom";
 export default function BenefitCard({ number, text }) {
   return (
     <div className="benefits__cards__wraper__card__over">
       <div className="benefits__cards__wraper__card">
         <div className="benefits__cards__wraper__card__number">{number}</div>
-        <div className="benefits__cards__wraper__card__text">{text}</div>
+        <Fade duration={1500}>
+          <div className="benefits__cards__wraper__card__text">{text}</div>
+        </Fade>
       </div>
     </div>
   );
